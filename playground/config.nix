3f44c0b88c18d85cbf17b90ad9ee1338ac7d2def
Ingolf Wanger <contact@ingolf-wagner.de>
2019-03-02 22:53:57 +0100

    improved the system

--- playground/config.nix
+++ playground/config.nix
@@ -1,18 +1,9 @@
 {...}:
 {
 
-  users.admins.palo.publicKey = "ssh-rsa asdfasdf ";
-
-  #hcloud.enable = true;
-
-  #resource.hcloud_server.nginx = {
-  #  name = "terranix.nginx";
-  #  image  = "debian-10";
-  #  server_type = "cx11";
-  #  backups = false;
-  #};
-
   imports = [ ./modules ];
+
+  users.admins.palo.publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC68sGJVgzmRqTm5l4qPWEkdVH53FP++0xRdCP0HK8OhZ1e0gC12Dj0AJO/06rqd8jB45Z+mdva8Y0nEhjhehGgUwsUUZO9bV/myptFlL2drqCnfG4rItKx34+HvnNJ321ylDxqIjjDit9ud3PX/Txiw463ION1lQJBzcMOGvoXaqn/3F+p4Bwfgt50GX8u+BVasFI5THiNVeCxIX5YvoCCnznnzln9GtbyzdSdCN48xyuEWjvBq7SAr80c2Rf6yl/YfnU3lfBSJpuxVFrLklxT7fpVxqZe2OzJmAlu5322M5EKaaZo8r8+RtHVe40WILneF2lJCk3X8ZYi422tiN8rlF/OlYELwzNMVjxohFwJxzkl3pm2CfuLz1cQ0AzHpDiv7MVzF+0cRRYxbX0+zORL9G3lRBGqqDhbyF1Ov8lNiHoXFIv+wc5DF8+fyw7qB083rXhTK7R7QfY9hGHHbs2Ep6m9Kfa31uuziYRtMcqSi7+9Lu926uECUWnzqrVgGrsFaF66xa7Z/uGj0cif2q2kVLnL1cHpRslF2VgK/l0NlMCn4MGjIGSfwga6wCgkjW4rlo78kezRcfveyNCtKTjt2A0hSzDnsx/9bqP/cyw+S1xtToqzh20x6LKUDDPDqJ+hmyAg++UccKDGoYsh/CvJe9w5UvHOfzTyvxRPylC/lQ== palo@pepe";
 
   servers.grafana.test = {};
 
--- playground/modules/grafana.nix
+++ playground/modules/grafana.nix
@@ -6,6 +6,13 @@ let
 
   cfg = config.servers.grafana;
 
+  allAdmins = 
+    if ( hasAttr "admins" config.users ) 
+    then
+      config.users.admins
+    else
+      {};
+
 in {
 
   options.servers.grafana = mkOption {
@@ -28,12 +35,15 @@ in {
       mkMerge [
         { resource.hcloud_server =
             let
+              allUsers = mapAttrsToList ( name: ignore: "\${ hcloud_ssh_key.grafana_${name}.id }" ) allAdmins;
+
               allResources = mapAttrs' ( name: configuration: {
                 name = "grafana_${name}";
                 value = {
                   name = configuration.name;
                   image  = "ubuntu-18.04";
                   server_type = "cx11";
+                  ssh_keys = allUsers;
                   user_data = ''
                     apt-get update
                     apt-get intstall docker
@@ -41,6 +51,35 @@ in {
                   backups = false;
                 };
               }) cfg;
+            in
+              allResources;
+
+          output = 
+            let
+              ipv4Address = mapAttrs' ( name: ignore: { 
+                name = "grafana_${name}_ipv4_address";
+                value = { value = "\${ hcloud_server.grafana_${name}.ipv4_address }"; } ;
+              } ) cfg;
+
+              ipv6Address = mapAttrs' ( name: ignore: { 
+                name = "grafana_${name}_ipv6_address";
+                value = { value = "\${ hcloud_server.grafana_${name}.ipv6_address }"; };
+              } ) cfg;
+            in
+              ipv4Address;
+            
+              
+        }
+        {
+          resource.hcloud_ssh_key = 
+          let
+              allResources = mapAttrs' ( name: configuration: {
+                name = "grafana_${name}";
+                value = {
+                  name = "Grafana SSH Key ${name}";
+                  public_key = configuration.publicKey;
+                };
+              }) allAdmins;
             in
               allResources;
         }
