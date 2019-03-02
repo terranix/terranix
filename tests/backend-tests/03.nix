2836f99d33e1bf95282d4d76ed3d57369f75c125
Ingolf Wanger <contact@ingolf-wagner.de>
2019-03-02 04:13:18 +0100

    add some more backend tests

---
+++ tests/backend-tests/03.nix
@@ -0,0 +1,14 @@
+{...}:
+{
+
+  remote_state.s3.test = {
+    bucket  = "some-where-over-the-rainbow";
+    key = "my-terraform-state.tfstate";
+    region = "eu-central-1";
+  };
+
+  remote_state.local.test = {
+    path = "some-where-over-the-rainbow";
+  };
+
+}
