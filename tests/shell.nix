c150872e4bc746552017151ac860f7cdad52dd3a
Ingolf Wanger <contact@ingolf-wagner.de>
2019-05-26 19:27:04 +0200

    test/shell.nix cleanup

--- tests/shell.nix
+++ tests/shell.nix
@@ -8,17 +8,6 @@ let
 
   terranix = pkgs.callPackages ../default.nix {};
 
-  terraformCurrent = pkgs.terraform.overrideAttrs( old: rec {
-    version = "0.11.10";
-    name = "terraform-${version}";
-    src = pkgs.fetchFromGitHub {
-      owner  = "hashicorp";
-      repo   = "terraform";
-        rev    = "v${version}";
-        sha256 = "08mapla89g106bvqr41zfd7l4ki55by6207qlxq9caiha54nx4nb";
-      };
-    });
-
   testFolder = folder:
     let
       ls = builtins.readDir folder;
@@ -48,19 +37,14 @@ let
 
 in pkgs.mkShell {
 
-  # needed pkgs
-  # -----------
   buildInputs = with pkgs; [
     terranix
-    #terraformCurrent
     terraform
     pup
     pandoc
     testScript
   ];
 
-  # run this on start
-  # -----------------
   shellHook = ''
     HISTFILE=${toString ./.}/.history
   '';
