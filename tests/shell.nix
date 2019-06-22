fe303ed2bc5cae7b3d2d55d422643bf7c2894f50
Ingolf Wanger <contact@ingolf-wagner.de>
2019-06-22 22:41:28 +0200

    test setup cleanup

--- tests/shell.nix
+++ tests/shell.nix
@@ -8,40 +8,22 @@ let
 
   terranix = pkgs.callPackages ../default.nix {};
 
-  test = import ./test.nix { inherit pkgs terranix; inherit (pkgs) lib; };
-
-  testFolder = folder:
-    let
-      ls = builtins.readDir folder;
-      files = builtins.attrNames (filterAttrs (file: fileType: fileType == "regular") ls );
-      nixFiles = builtins.filter (hasSuffix "nix") files;
-
-      # possible
-      script = file: ''
-        @test "Testing : ${folder}/${file}" {
-          run ${terranix}/bin/terranix --quiet ${folder}/${file}
-          [ "$status" -eq 0 ]
-          [ "$output" =  ${escapeShellArg (fileContents "${folder}/${file}.output")} ]
-        }
-      '';
-
-      batsScripts =  map (text: pkgs.writeText "test" text) test;
-      allScripts = map (file: "${pkgs.bats}/bin/bats ${file}") batsScripts;
-    in
-    pkgs.writeScript "script" (concatStringsSep "\n" allScripts);
+  createTest = testimport:
+  let
+    test = import testimport  { inherit pkgs terranix; inherit (pkgs) lib; };
+    batsScripts =  map (text: pkgs.writeText "test" text) test;
+    allScripts = map (file: "${pkgs.bats}/bin/bats ${file}") batsScripts;
+  in
+  pkgs.writeScript "test-script" (concatStringsSep "\n" allScripts);
 
   testScript =
     let
-      testFolders = attrNames (filterAttrs (file: fileType: fileType == "directory") (readDir (toString ./.)));
-      fullTestFolders = map (file: "${toString ./.}/${file}") testFolders;
+      allTests = [ (createTest ./test.nix) ];
     in
       pkgs.writeShellScriptBin "test-terranix" ''
         set -e
-        ${concatStringsSep "\n" (map testFolder fullTestFolders)}
+        ${concatStringsSep "\n" allTests}
       '';
-
-
-
 
 in pkgs.mkShell {
 
