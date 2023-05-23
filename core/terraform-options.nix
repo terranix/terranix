dca863a2d08dc7a2b30eaf7bf04d347bb1c3aaed
mdarocha <git@mdarocha.pl>
2023-05-23 19:31:14 +0200

    Fixup referenceable functor

--- core/terraform-options.nix
+++ core/terraform-options.nix
@@ -29,9 +29,14 @@ let
 
   mkReferenceableOption = { ... }@args:
     mkMagicMergeOption (args // {
-      apply = mapAttrs (type: v1:
-        mapAttrs (label: v2:
-          v2 // { __functor = self: attr: "\${${type}.${label}.${attr}}"; })
+      apply = let
+        mapAttrsOrSkip = f: attrs:
+          if isAttrs attrs then mapAttrs f attrs else attrs;
+      in mapAttrsOrSkip (type: v1:
+        mapAttrsOrSkip (label: v2:
+          if isAttrs v2
+          then v2 // { __functor = self: attr: "\${${type}.${label}.${attr}}"; }
+          else v2)
         v1);
     });
 in
