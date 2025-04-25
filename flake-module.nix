c2ec64292b10dfa3e131dc737595da4a2ef5207a
Terje Larsen <terlar@gmail.com>
2025-04-25 22:41:58 +0200

    fix: Ensure workspace exists in flake-parts module scripts (#119)

--- flake-module.nix
+++ flake-module.nix
@@ -142,6 +142,7 @@
                                     inherit name;
                                     runtimeInputs = [ submod.config.result.terraformWrapper ];
                                     text = ''
+                                      mkdir -p ${submod.config.workdir}
                                       ln -sf ${submod.config.result.terraformConfiguration} ${submod.config.workdir}/config.tf.json
                                       ${text}
                                     '';
