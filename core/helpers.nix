8d862fdfe831b328c392ae9c13deffe3b8dc0d87
Carl Andersson <git@lillecarl.com>
2025-08-03 11:23:15 +0200

    feat(helpers): add interpolation escaping utilities
    Add tf.mapStringsDeep, tf.escapeInterpolation, and tf.escapeInterpolationDeep
    to handle text from third-party sources containing ${} and %{} sequences.
    
    - mapStringsDeep: recursively applies function to all strings in nested attrs/lists
    - escapeInterpolation: escapes ${} and %{} to prevent Nix/Terraform interpolation
    - escapeInterpolationDeep: combines both for complete attrset processing
    - tf attrset now recursive to allow internal function references

--- core/helpers.nix
+++ core/helpers.nix
@@ -1,7 +1,7 @@
 # override to the standard nixpkgs lib, adding
 # terranix-related helpers
 pkgs: self: super: {
-  tf = {
+  tf = rec {
     # escape ${} to create a terraform reference string
     ref = ref: "\${${ref}}";
 
@@ -20,6 +20,38 @@ pkgs: self: super: {
             else source;
         in
         "templatefile(\"${template}\", ${builtins.toJSON variables})";
+
+    # Apply function to all strings in an attrset recursively through lists as well
+    # Converts stringlike types to string before (derivations become paths for example)
+    mapStringsDeep =
+      function: value:
+      if super.isStringLike value then
+        function (toString value)
+      else if super.isAttrs value then
+        super.mapAttrs (name: mapStringsDeep function) value
+      else if super.isList value then
+        map (mapStringsDeep function) value
+      else
+        value;
+
+    # Escapes strings so variables aren't interpolated with ${} and %{}
+    escapeInterpolation =
+      value:
+      super.replaceStrings
+        [
+          # Nix and tf shares this escape pattern
+          "\${"
+          # tf also interpolates %{}
+          "%{"
+        ]
+        [
+          "$\${"
+          "%%{"
+        ]
+        value;
+
+    # Escape all strings in an attrset so tf can parse it properly
+    escapeInterpolationDeep = attrs: mapStringsDeep escapeInterpolation attrs;
   };
 
   # top-level aliases for backwards compatibility
